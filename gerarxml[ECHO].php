1634d5d0725cf7a786011a8f54400ed3004eaf52
Gambozo <Gambozo@users.noreply.github.com>
2020-03-16 15:42:07 -0300

    Update gerarxml[ECHO].php

--- gerarxml[ECHO].php
+++ gerarxml[ECHO].php
@@ -10,7 +10,7 @@ function htmlspecial($string)
 };
 function tirarAcentos($string)
 {
-    return preg_replace(array("/(á|à|ã|â|ä)/", "/(Á|À|Ã|Â|Ä)/", "/(é|è|ê|ë)/", "/(É|È|Ê|Ë)/", "/(í|ì|î|ï)/", "/(Í|Ì|Î|Ï)/", "/(ó|ò|õ|ô|ö)/", "/(Ó|Ò|Õ|Ô|Ö)/", "/(ú|ù|û|ü)/", "/(Ú|Ù|Û|Ü)/", "/(ñ)/", "/(Ñ)/"), explode(" ", "a A e E i I o O u U n N"), $string);
+    return preg_replace(array("/(á|à|ã|â|ä)/", "/(Á|À|Ã|Â|Ä)/", "/(é|è|ê|ë)/", "/(É|È|Ê|Ë)/", "/(í|ì|î|ï)/", "/(Í|Ì|Î|Ï)/", "/(ó|ò|õ|ô|ö)/", "/(Ó|Ò|Õ|Ô|Ö)/", "/(ú|ù|û|ü)/", "/(Ú|Ù|Û|Ü)/", "/(ñ)/", "/(Ñ)/", "/(ç)/", "/(Ç)/"), explode(" ", "a A e E i I o O u U n N c C"), $string);
 };
 // Config
 $domain = 'http://www.sicadi.com.br/';
@@ -42,17 +42,14 @@ foreach ($xml->imovel as $estate) {
     $caracteristicas = htmlspecial($details->caracteristicas);
     $coordenadas = $details->coordenadas;
 
-    echo '<imovel id="' . $id . '" codigo="' . $codigo . '">';
-    echo '<tipo>' . $tipo . '</tipo>';
-    echo '<bairro>' . $bairro . '</bairro>';
-    echo '<garagens>' . $garagens . '</garagens>';
-    echo '<endereco>' . $endereco . '</endereco>';
+    echo '<codigo cod="' . $codigo . '" idImv="' . $id . '">';
+    echo '<tipo><![CDATA[ ' . $tipo . ' ]]></tipo>';
     echo '<disponibilidade>';
     foreach ($estate->disponibilidade->disponivel as $availability) {
         $finalidade = $availability->attributes()->finalidade;
         $valor = $availability->attributes()->valor;
         $valorcond = $valor == '0,00' ? 'cond="Consulte!"' : '';
-        echo '<disponivel finalidade="' . $finalidade . '" valor="' . $valor . '" ' . $valorcond . '/>';
+        echo '<disponivel finalidade="' . strtoupper($finalidade) . '" valor="' . $valor . '" ' . $valorcond . '/>';
     }
     echo '</disponibilidade>';
     if ($numfotos >= 1) {
@@ -71,12 +68,15 @@ foreach ($xml->imovel as $estate) {
         echo '<img url="' . $foto . '">SEM IMAGEM</img>';
         echo '</imagens>';
     }
+    echo '<coordenadas>' . $coordenadas . '</coordenadas>';
+    echo '<endereco><![CDATA[ ' . $endereco . ' ]]></endereco>';
+    echo '<bairro><![CDATA[ ' . $bairro . ' ]]></bairro>';
+    echo '<garagens>' . $garagens . '</garagens>';
     echo '<cidade>' . $cidade . '</cidade>';
     echo '<estado>' . $estado . '</estado>';
     echo '<cep>' . $cep . '</cep>';
     echo '<condominio>' . $condominio . '</condominio>';
     echo '<caracteristicas><![CDATA[' . $caracteristicas . ']]></caracteristicas>';
-    echo '<coordenadas>' . $coordenadas . '</coordenadas>';
     echo '<composicao>';
     foreach ($details->composicao_imovel->composicao as $composicao) {
         $qnt_comp = htmlspecial($composicao->attributes()->quantidade);
@@ -84,7 +84,6 @@ foreach ($xml->imovel as $estate) {
         echo '<' . $item_comp . '>' . $qnt_comp . '</' . $item_comp . '>';
     }
     echo '</composicao>';
-
-    echo '</imovel>';
+    echo '</codigo>';
 }
 echo '</imoveis>';
